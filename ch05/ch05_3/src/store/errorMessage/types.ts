28bc6aa8e93a065fa3af36d570e1fb7826909873
kyoungjini <whrudwls0418@naver.com>
2023-11-27 00:39:36 +0900

    week6

---
+++ ch05/ch05_3/src/store/errorMessage/types.ts
@@ -0,0 +1,8 @@
+import type {Action} from 'redux'
+
+export type State = string
+
+export type SetErrorMessageAction = Action<'@errorMessage/setErrorMessage'> & {
+  payload: State
+}
+export type Actions = SetErrorMessageAction
